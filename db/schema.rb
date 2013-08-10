36c98f19e6c7b289a4fc00ce2f9b2b862787b9c3
Scott Semple <scott@scottsemple.com>
2013-08-10 13:24:32 -0600

    Rollback creator_id column migration to user_id.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130810163459) do
+ActiveRecord::Schema.define(version: 20130810133702) do
 
   create_table "categories", force: true do |t|
     t.string "name"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20130810163459) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "creator_id"
+    t.integer  "user_id"
   end
 
   create_table "posts", force: true do |t|
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20130810163459) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "creator_id"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
