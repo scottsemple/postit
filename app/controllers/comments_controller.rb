d5c3becef164a18eb4f9838fa1a51d208e1f5b93
Scott Semple <scott@scottsemple.com>
2013-08-19 15:23:58 -0600

    Push comments addition.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,14 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:create]
 
-  # POST /comments
-  # POST /comments.json
   def create
-    @comment = Comment.new(post_params) # use post_params because a comment is
-                                        # attached to a post?
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(content: params[:comment][:content])
+    @comment.post = @post
 
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @comment }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    if @comment.save
+      redirect_to post_path(@post), notice: 'Comment was successfully created.'
+    else
+      render 'posts/show'
     end
   end
 
